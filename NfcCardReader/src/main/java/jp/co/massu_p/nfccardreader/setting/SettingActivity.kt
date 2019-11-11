930c7b70f689204416a6a1b882bd651e05117012
massu_p <toratanf91@gmail.com>
2019-11-11 22:19:47 +0900

    文字コード定義、改行コード定義を修正。

--- NfcCardReader/src/main/java/jp/co/massu_p/nfccardreader/setting/SettingActivity.kt
+++ NfcCardReader/src/main/java/jp/co/massu_p/nfccardreader/setting/SettingActivity.kt
@@ -2,7 +2,6 @@ package jp.co.massu_p.nfccardreader.setting
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,8 +15,6 @@ import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
-	lateinit private var pref : SharedPreferences
-
 	companion object {
 		fun intent(context: Context): Intent =
 			Intent(context, SettingActivity::class.java)
@@ -27,23 +24,21 @@ class SettingActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_setting)
 
-		pref = getSharedPreferences(getString(R.string.pref_name_file_format), Context.MODE_PRIVATE)
-		val charset = Charset(pref.getString(getString(R.string.pref_key_charset), null))
-		val linefeed = LineFeed(pref.getString(getString(R.string.pref_key_linefeed), null))
+		val charset = Charset.getCurrent(applicationContext)
+		val linefeed = LineFeed.getCurrent(applicationContext)
 
-
-		val charsetAdapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, Charset.displayNames())
+		val charsetAdapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, Charset.getDisplayNames())
 		charsetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 		spinner_charset.adapter = charsetAdapter
-		spinner_charset.setSelection(charset.ordinal())
+		spinner_charset.setSelection(charset.ordinal)
 		spinner_charset.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 			override fun onNothingSelected(parent: AdapterView<*>?) {
 			}
 
 			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-				val parentSpinner = parent as Spinner
-				val selectedCharset = Charset(parentSpinner.selectedItem as String)
-				pref.edit().putString(getString(R.string.pref_key_charset), selectedCharset.type.name).apply()
+				val parentSpinner = parent as Spinner // TODO 確認
+				val selectedCharset = Charset.values()[position]
+				Charset.setCurrent(applicationContext, selectedCharset)
 			}
 
 		}
@@ -51,15 +46,15 @@ class SettingActivity : AppCompatActivity() {
 		val linefeedAdapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, LineFeed.displayNames())
 		charsetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 		spinner_linefeed.adapter = linefeedAdapter
-		spinner_linefeed.setSelection(linefeed.ordinal())
+		spinner_linefeed.setSelection(linefeed.ordinal)
 		spinner_linefeed.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
 			override fun onNothingSelected(parent: AdapterView<*>?) {
 			}
 
 			override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-				val parentSpinner = parent as Spinner
-				val selectedLineFeed = LineFeed(parentSpinner.selectedItem as String)
-				pref.edit().putString(getString(R.string.pref_key_linefeed), selectedLineFeed.type.name).apply()
+				val parentSpinner = parent as Spinner // TODO 確認
+				val selectedLineFeed = LineFeed.values()[position]
+				LineFeed.setCurrent(applicationContext, selectedLineFeed)
 			}
 
 		}
