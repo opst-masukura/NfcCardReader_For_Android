6efd63ac16045fa2250426503f6beaa9a19aee77
masukura <masukura.yutaka@opst.co.jp>
2019-11-12 16:13:35 +0900

    トップ画面のレイアウト作成

--- NfcCardReader/src/main/java/jp/co/massu_p/nfccardreader/mainview/MainActivity.kt
+++ NfcCardReader/src/main/java/jp/co/massu_p/nfccardreader/mainview/MainActivity.kt
@@ -9,5 +9,11 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		if (savedInstanceState == null) {
+			val transaction = supportFragmentManager.beginTransaction()
+			transaction.add(R.id.view_container, MainFragment.newInstance())
+			transaction.commit()
+		}
 	}
 }
